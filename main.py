b5a8600a98bade164b50329f0e47f2ece423a30c
TiaanVenter <cplkane@gmail.com>
2026-01-01 14:48:31 +0200

    CH2 L6 solved

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
+from stats import num_words, get_num_char
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
     return file_contents
 
-def num_words(file_contents):
-    words = 0
-    for i in file_contents.split():
-        words += 1
-    return words
-
 def main():
-    print(f"Found {num_words(get_book_text('books/frankenstein.txt'))} total words <commit>")
+    print(f"Found {num_words(get_book_text('books/frankenstein.txt'))} total words")
+    print(f"{get_num_char(get_book_text('books/frankenstein.txt'))}")
 
 main()
---
+++ stats.py
@@ -0,0 +1,15 @@
+def num_words(file_contents):
+    words = 0
+    for i in file_contents.split():
+        words += 1
+    return words
+
+def get_num_char(file_contents):
+    input_string = file_contents.lower()
+    char_counts = {}
+    for i in input_string:
+        if i not in char_counts:
+            char_counts[i] = 1
+        else:
+            char_counts[i] += 1
+    return char_counts
