056e153676c7a86daae4ef7c4ae0a50c06048136
TiaanVenter <cplkane@gmail.com>
2026-01-02 10:34:43 +0200

    CH3 L2 solved; completed the course!

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
+import sys
 from stats import num_words, get_num_char, sorted_list
+
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+path_to_file = sys.argv[1]
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
     return file_contents
 
-
-
 def main():
-    sorted_chars = sorted_list(get_num_char(get_book_text('books/frankenstein.txt')))
-    book_text = get_book_text("books/frankenstein.txt")
+    sorted_chars = sorted_list(get_num_char(get_book_text(path_to_file)))
+    book_text = get_book_text(path_to_file)
 
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {path_to_file}")
     print("----------- Word Count ----------")
     print(f"Found {num_words(book_text)} total words")
     print("--------- Character Count -------")
 
     for i in sorted_chars:
-        if i["char"].isalpha() == True:
+        if i["char"].isalpha():
             print(f"{i['char']}: {i['num']}")
 
 main()
