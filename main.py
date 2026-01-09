3c72db2af5584d1e461fac72d2db02e9ff75828a
Tiaan Venter <72299253+TiaanVenter@users.noreply.github.com>
2026-01-09 14:44:28 +0200

    Update main.py
    Added comments, explaining each block of code

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import sys
 from stats import num_words, get_num_char, sorted_list
 
+# This loop checks whether enough arguments have been passed when trying to run the main file.
 if len(sys.argv) < 2:
     print("Usage: python3 main.py <path_to_book>")
     sys.exit(1)
 
+# Assign the 2nd argument to be the path_to_file variable
 path_to_file = sys.argv[1]
 
+# Open the file, read its contents into a variable called file_contents
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
     return file_contents
 
+# Define the main function: call the functions defined in stats.py, print them out; prints out what book is being analyzed, the total number of words, and the character count.
 def main():
     sorted_chars = sorted_list(get_num_char(get_book_text(path_to_file)))
     book_text = get_book_text(path_to_file)
@@ -26,4 +30,5 @@ def main():
         if i["char"].isalpha():
             print(f"{i['char']}: {i['num']}")
 
+# Execute main function
 main()
